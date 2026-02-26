e22b08ff06e30f1a08682350d81fb23a42cc0763
Kevin.M <kema2501@student.miun.se>
2026-02-26 09:18:47 +0100

    Skapar cirkeldiagram med 5 mest sökta program

--- src/js/main.js
+++ src/js/main.js
@@ -1,86 +1,95 @@
+"use strict";
+
 document.addEventListener("DOMContentLoaded", () => { 
     fetchAdmissionData();
 
-if(document.getElementById("animation")) {
-const animationButton = document.getElementById("show-animation");
-animationButton.addEventListener("click", displayAnimation);
+    if(document.getElementById("animation")) {
+    const animationButton = document.getElementById("show-animation");
+    animationButton.addEventListener("click", displayAnimation);
 
-function displayAnimation() {
-    const gif = document.querySelector(".gif");
+    function displayAnimation() {
+        const gif = document.querySelector(".gif");
 
-        gif.style.animationPlayState = "running"
-  }
-}
-
-// Funktion för att hämta antagningsstatistik
-async function fetchAdmissionData() {
-    const url = "https://mallarmiun.github.io/Frontend-baserad-webbutveckling/Moment%205%20-%20Dynamiska%20webbplatser/statistik_sokande_ht25.json";
-
-    // Anropa och läs ut data
-    try {
-        const response = await fetch(url);
-        const admissions = await response.json();
-
-        topCourses(admissions);
-        topPrograms(admissions);
-
-    } catch (error){
-        console.error("Fel: " + error);
+            gif.style.animationPlayState = "running"
     }
-}
+    }
 
-    // Lista ut top 6 mest sökta kurser
-function  topCourses(admissions) {
-    let courses = admissions.filter((admission) => admission.type === "Kurs");
-    courses.sort((a, b) => b.applicantsTotal - a.applicantsTotal);
-    courses = courses.slice(0,6);
+    // Funktion för att hämta antagningsstatistik
+    async function fetchAdmissionData() {
+        const url = "https://mallarmiun.github.io/Frontend-baserad-webbutveckling/Moment%205%20-%20Dynamiska%20webbplatser/statistik_sokande_ht25.json";
 
-    createChart(courses)  
-}
+        // Anropa och läs ut data
+        try {
+            const response = await fetch(url);
+            const admissions = await response.json();
 
-    //Lista ut top 5 mest sökta program
+            topCourses(admissions);
+            topPrograms(admissions);
 
-
-function createChart(courses) {
-    const ctx = document.getElementById('barChart');
-
-    new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: courses.map(row => row.name),
-        datasets: [{
-        label: 'Antal sökande',
-        data: courses.map(row => row.applicantsTotal),
-        borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            y: {
-                beginAtZero: true
-            }
+        } catch (error){
+            console.error("Fel: " + error);
         }
     }
-    });
-}
 
-createPieChart();
+    // Lista top 6 mest sökta kurser
+    function  topCourses(admissions) {
+        let courses = admissions.filter((admission) => admission.type === "Kurs");
+        courses.sort((a, b) => b.applicantsTotal - a.applicantsTotal);
+        courses = courses.slice(0,6);
 
-function createPieChart() {
-    const pieChart = document.getElementById('pieChart');
-
-    new Chart(pieChart, {
-    type: 'pie',
-    data: {
-        labels: ['Red', 'Blue', 'Yellow'],
-        datasets: [{
-        label: 'My First Dataset',
-        data: [300, 50, 100],
-        backgroundColor: ['rgb(255, 99, 132)', 'rgb(54, 162, 235)', 'rgb(255, 205, 86)'],
-        hoverOffset: 4
-        }]
+        createChart(courses)  
     }
-    });
-}
 
+    // Skapa stapeldiagram med mest sökta kurser
+    function createChart(courses) {
+        const barChart = document.getElementById('barChart');
+
+        new Chart(barChart, {
+        type: 'bar',
+        data: {
+            labels: courses.map(row => row.name),
+            datasets: [{
+            label: 'Antal sökande',
+            data: courses.map(row => row.applicantsTotal),
+            borderWidth: 1,
+            backgroundColor: '#e55934ff',
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+        });
+    }
+
+    //Lista top 5 mest sökta program
+    function topPrograms(admissions) {
+
+        let programs = admissions.filter((admission) => admission.type === "Program");
+        programs.sort((a, b) => b.applicantsTotal - a.applicantsTotal);
+        programs = programs.slice(0,5);
+
+        createPieChart(programs);
+    }
+
+    // Skapa cirkeldiagram med mest sökta program
+    function createPieChart(programs) {
+        const pieChart = document.getElementById('pieChart');
+
+        new Chart(pieChart, {
+        type: 'pie',
+        data: {
+            labels: programs.map(row => row.name),
+            datasets: [{
+            label: 'Antal sökande',
+            data: programs.map(row => row.applicantsTotal),
+            backgroundColor: ['#e55934ff', '#d9594cff', '#ce8d66ff', '#c3bf6dff', '#b7b868ff'],
+            hoverOffset: 4
+            }]
+        }
+        });
+    }
 });
