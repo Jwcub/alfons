35b85d445693cd95b1d4b160111c7076a4f4d028
Kevin.M <kema2501@student.miun.se>
2026-02-26 19:25:39 +0100

    Omvandlar kordinater till decimaltal och visar på karta

--- src/js/map-script.js
+++ src/js/map-script.js
@@ -4,6 +4,11 @@ document.addEventListener("DOMContentLoaded", () => {
     document.getElementById("search-btn").addEventListener("click", findAdress);
 });
 
+    /** 
+     * Hämtar förslag på adresser utifrån sökning. Skapar en array med alternativ och skickar med funktionen findOnMap. 
+     * @async
+     */
+
 async function findAdress() {
     const inputValue = document.getElementById("search-input").value;
     let searchPhrase = inputValue.replace(" ", "+");
@@ -20,14 +25,26 @@ async function findAdress() {
     }
 }
 
+    /** 
+     * Plockar ut kordinaterna för första platsen i listan och visar kordinaterna på karta i DOM. 
+     * @param - Array med förslag på platser utifrån sökning
+     */
+
 function findOnMap(location) {
 
-    const lat = location[0].lat;
-    const long = location[0].lon;
+    // Omvandlar latitud till decimaltal och sätter värden för min/max latitud
+    const lat = parseFloat(location[0].lat);
+    const lat1 = lat - 0.05;
+    const lat2 = lat + 0.05;
 
-    console.log(lat + long);
-
-    
-
-
+    // Omvandlar longitud till decimaltal och sätter värden för min/max longitud
+    const long = parseFloat(location[0].lon);
+    const long1 = long - 0.05;
+    const long2 = long + 0.05;
+  
+    // Ersätter karta med uppdaterad vy
+    const mapEl = document.getElementById("map");
+    mapEl.innerHTML = `
+    <iframe width="425" height="350" class="map" src="https://www.openstreetmap.org/export/embed.html?bbox=${long1}%2C${lat1}%2C${long2}%2C${lat2}&layer=mapnik&marker=${lat}%2C${long}" style="border: 1px solid black"></iframe>
+    `;
 }
